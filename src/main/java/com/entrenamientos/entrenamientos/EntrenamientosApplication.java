61da537eb5f4f7e7ab976d5a7a27a3f031f841f1
Cristian <230183@fppiramide.com>
2023-10-23 09:10:43 +0200

    Initial

---
+++ src/main/java/com/entrenamientos/entrenamientos/EntrenamientosApplication.java
@@ -0,0 +1,13 @@
+package com.entrenamientos.entrenamientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrenamientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrenamientosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entrenamientos/entrenamientos/EntrenamientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrenamientos.entrenamientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrenamientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
